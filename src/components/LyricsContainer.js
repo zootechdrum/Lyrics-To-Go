bc02f43f2bbf131d651a64274d798ee3dca1c273
Cesar Gomez <zootechdrum@gmail.com>
2020-08-15 22:27:58 -0700

    removed style tag from lyricsContainer

--- src/components/LyricsContainer.js
+++ src/components/LyricsContainer.js
@@ -6,8 +6,10 @@ export default class LyricsContainer extends Component {
       <div className="clearSearch">
         <div className="row">
           <div
-            style={{ display: this.props.lyrics ? "block" : "none" }}
-            className={"col-md-6 mb-3 lyrics "}
+            className={
+              ({ display: this.props.lyrics ? "block" : "none" },
+              " col-md-6 mb-3 lyrics ")
+            }
           >
             <p>{this.props.lyrics}</p>
           </div>
